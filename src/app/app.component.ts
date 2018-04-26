91c0563cc6902a09136579874360fe4d696a3d60
Helen Triolo <helentriolo@localfirstmedia.com>
2018-04-26 18:02:54 -0400

    Starting point for angular-tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  // declare AppComponent's properties here:
+
+  public title: string;
+  public myBooks: Array<string>;         // same as myBooks: string[];
+  public myContacts: Array<any>;        // use any instead of object to avoid errors accessing properties
+
+  // initialize AppComponent properties here:
+
+  constructor() {
+    this.title = 'An Angular Javascript Practice App';
+    this.myBooks = ['Angular Pro', 'React Pro', 'Javascript ES6'];
+    this.myContacts = [
+      {
+        firstname: 'Joe',
+        lastname: 'Poppenhurst',
+        gender: 'M',
+        address: '357 Spring St',
+        city: 'Cleveland',
+        state: 'OH'
+      },
+      {
+        firstname: 'Jane',
+        lastname: 'Pringle',
+        gender: 'F',
+        address: '322 Main St',
+        city: 'Philadelphia',
+        state: 'PA'
+      },
+      {
+        firstname: 'Jake',
+        lastname: 'Palau',
+        gender: 'M',
+        address: '738 Indigo Ave',
+        city: 'Silver Spring',
+        state: 'MD'
+      }
+    ];
+
+  }
 }
+
